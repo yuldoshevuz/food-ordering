8f873d6b4013fbd31e8b560f6d0938e1195c6a89
Muhammadali <mukhammadaliweb@gmail.com>
2024-07-13 18:47:23 +0500

    Some bugs fixed

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,20 +1,11 @@
 import { Router } from "express";
 
-import swaggerUi from "swagger-ui-express";
-import swaggerDocs from "../data/swagger.json"
 import environments from "../config/environments";
 import adminRouter from "./admin.routes";
 import vendorRouter from "./vendor.routes";
 
 const router = Router()
 
-const swaggerDocument = JSON.stringify(swaggerDocs)
-    .replace("$host", environments.HOST || `localhost:${environments.PORT}`);
-
-router.use("/docs",
-    swaggerUi.serve,
-    swaggerUi.setup(JSON.parse(swaggerDocument))
-); 
 router.use("/admin", adminRouter)
 router.use("/vendor", vendorRouter)
 
